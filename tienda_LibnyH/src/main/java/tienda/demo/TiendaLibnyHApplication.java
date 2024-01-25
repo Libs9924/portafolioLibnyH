56132f9fedadc561482ff6ed981fdcfc35bd86f0
Libs9924 <lhernandez90977@ufide.ac.cr>
2024-01-24 18:33:57 -0600

    Creación de proyecto

---
+++ tienda_LibnyH/src/main/java/tienda/demo/TiendaLibnyHApplication.java
@@ -0,0 +1,13 @@
+package tienda.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaLibnyHApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaLibnyHApplication.class, args);
+	}
+
+}
